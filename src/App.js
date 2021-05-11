1a9f580ad97d97c8853dd6d404315bd4a2aa8120
Alex Danciu <alex.boos.123441@gmail.com>
2021-05-11 22:26:02 +0300

    https

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
           <Route path="/:city">
             <Currect />
           </Route>
-          <Route path="*" component={Page404}/>
         </Switch>
         <Footer />
       </BrowserRouter>
--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,9 @@
 
 const urls = (city , lon , lat)=>{
     return{
-        firstUrl:`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`,
+        firstUrl:`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`,
         secondUrl:`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`,
-        thirdUrl:`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`
+        thirdUrl:`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`
     }
 }
 
